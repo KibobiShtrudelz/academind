d40984ec2202bdbb1ec9e25f02bae386064cc8e2
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2024-02-20 18:24:11 +0200

    update projects folder structure

--- next-with-file-based-routing/nextjs-course-code/pages/clients/[id]/index.js
+++
@@ -1,7 +0,0 @@
-export default function ClientProjectsPage() {
-  return (
-    <div>
-      <h1>The Projects of a Given Client</h1>
-    </div>
-  );
-}
--- next-with-file-based-routing/nextjs-course-code/pages/portfolio/index.js
+++
@@ -1,8 +0,0 @@
-export default function PortfolioPage() {
-  return (
-    <div>
-      <h1>Portfolio</h1>
-      <p>Projects I've worked on</p>
-    </div>
-  );
-}
