002d72570e2427013ffccc70c91e995df6460ecd
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2024-02-20 19:08:13 +0200

    finished 157.

---
+++ next-with-file-based-routing-project/pages/events/[...slug].js
@@ -0,0 +1,8 @@
+export default function FilteredEventsPage({ params }) {
+  console.log("params", params);
+  return (
+    <div>
+      <h1>Filtered Events</h1>
+    </div>
+  );
+}
---
+++ next-with-file-based-routing-project/pages/events/[eventId].js
@@ -0,0 +1,8 @@
+export default function EventDetailPage({ params }) {
+  console.log("params", params);
+  return (
+    <div>
+      <h1>Event Detail</h1>
+    </div>
+  );
+}
