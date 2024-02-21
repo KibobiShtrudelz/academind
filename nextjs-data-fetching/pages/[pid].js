ffd97f133a4a3309c00e827cffb787e955dacea8
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2024-02-21 22:29:40 +0200

    finished 187.

--- nextjs-data-fetching/pages/[pid].js
+++ nextjs-data-fetching/pages/[pid].js
@@ -17,31 +17,46 @@ export default function ProductDetailPage({ loadedProduct }) {
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
-  const productId = params.pid;
-
+async function getData() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+
+  const data = await getData();
+  const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return { notFound: true };
+  }
+
   return {
     props: {
-      loadedProduct: data.products.find((product) => product.id === productId),
+      loadedProduct: product,
     },
   };
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+
   return {
     // задължително име на ключа "paths"
-    paths: [
-      // задължително име на ключа "params"
-      { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
-    ],
-    fallback: false,
+    paths: ids?.map((id) => ({ params: { pid: id } })) || [],
+    // [
+    //   // задължително име на ключа "params"
+    //   { params: { pid: "p1" } },
+    //   // { params: { pid: "p2" } },
+    //   // { params: { pid: "p3" } },
+    // ],
+    fallback: true,
   };
 }
 
@@ -49,4 +64,8 @@ export async function getStaticPaths() {
  * По подразбиране Next НЕ генерира предварително страници, чиито имена на файовете са с квадратни скоби,
  * защото са динамични и няма как да знае колко точно трябва да генерира,
  * затова трябва да ползваме getStaticPaths.
+ *
+ * @ fallback: "blocking" - ако е "blocking" Next изчаква да се генерира цялата страница и чак тогава я показва на потребителя.
+ * @ fallback: "true" - ако е "true" Next показва страницата на потребителя, докато се генерира.
+ *    Ако е true трябва да добавим проверка дали props са заредили и ако не са - да покажем някакво съобщение или спинър, иначе апа гърми с undefined.
  */
