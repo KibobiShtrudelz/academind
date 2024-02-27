6768e3a4d8dabea448d1d54f2739c8d7470cdf04
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2024-02-28 00:56:34 +0200

    finished 243.

--- nextjs-course-code-08-prj-api-routes-starting-project/pages/api/comments/[eventId].js
+++ nextjs-course-code-08-prj-api-routes-starting-project/pages/api/comments/[eventId].js
@@ -1,38 +1,53 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   const eventId = req.query.eventId;
 
-  if (req.method === "GET") {
-    const dummyList = [
-      { id: "c1", name: "Max", text: "A first comment!" },
-      { id: "c2", name: "Manuel", text: "A second comment!" },
-    ];
-
-    res.status(200).json({ comments: dummyList });
-  }
+  const client = await MongoClient.connect(
+    "mongodb+srv://nextjs:Password101@nextjscluster.elpv2sz.mongodb.net/?retryWrites=true&w=majority&appName=NextJSCluster"
+  );
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
 
     if (
-      !email.includes("@") ||
+      !email?.includes("@") ||
       !name ||
-      name.trim() === "" ||
+      name?.trim() === "" ||
       !text ||
-      text.trim() === ""
+      text?.trim() === ""
     ) {
       res.status(422).json({ message: "Invalid input." });
       return;
     }
 
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId,
     };
 
-    console.log("newComment >>>", newComment);
+    const db = client.db("events");
 
-    res.status(201).json({ message: "Added comment.", comment: newComment });
+    const result = await db.collection("comments").insertOne({ newComment });
+
+    console.log("result", result);
+
+    newComment.id = result.insertedId;
+
+    if (req.method === "GET") {
+      const db = client.db();
+      const documents = await db
+        .collection("comments")
+        .find()
+        .sort({ _id: -1 })
+        .toArray();
+      console.log("documents", documents);
+
+      res.status(201).json({ message: "Added comment.", comment: documents });
+    }
+
+    client.close(); // close the connection
   }
 }
